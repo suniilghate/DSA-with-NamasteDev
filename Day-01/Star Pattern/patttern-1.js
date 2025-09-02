2a1c002a82518c2092f2785570bade5e530f5e06
Suniil Ghate <sunil.82ghate@gmail.com>
2025-09-02 19:23:41 +0530

    Star Patterns

---
+++ Day-01/Star Pattern/patttern-1.js
@@ -0,0 +1,16 @@
+/**
+ * Draw a 4 x 4 Star pattern
+ *              * * * *
+ *              * * * *
+ *              * * * *
+ *              * * * *
+ */
+
+let n = 4;
+for(let i = 0; i < n; i++) {
+    let row = "";
+    for(let j = 0; j < n; j++) {
+        row = row + " * ";
+    }
+    console.log(row);
+}
