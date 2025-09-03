18f19041e2462a2065dbc375c5e61957e5858b1f
Suniil Ghate <sunil.82ghate@gmail.com>
2025-09-03 12:46:52 +0530

    Triangle star pattern variation - left side

---
+++ Day-02/Star Pattern/pattern-01.js
@@ -0,0 +1,20 @@
+/**
+ *      Draw the following pattern
+ *      - - - *
+ *      - - * *
+ *      - * * *
+ *      * * * *
+ */
+
+let n = 4;
+
+for(let i = 0; i < n; i++){
+    let row = "";
+    for(let j = 0; j < n - (i + 1); j++) {
+        row = row + "   ";
+    }
+    for(let k = 0; k < i + 1; k++){
+        row = row + " * ";
+    }
+    console.log(row);
+}
