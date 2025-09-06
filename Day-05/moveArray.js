4727da52f2ddb2e321b314a0eeadec519893820b
Suniil Ghate <sunil.82ghate@gmail.com>
2025-09-06 15:23:02 +0530

    Move Arrays

---
+++ Day-05/moveArray.js
@@ -0,0 +1,25 @@
+/**
+ * Given an integer numsay nums, move all 0's to the end of it while maintaining the relative order of the 
+ * non-zero elements. Note that you must do this in-place without making a copy of the numsay.
+
+ */
+
+let nums = [0, 1, 0, 3, 12];
+
+let moveZero = function(nums) {
+    let x = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+    for(let i = x; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+    return nums;
+}
+
+console.log(moveZero(nums));
